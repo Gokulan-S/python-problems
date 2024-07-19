0ff202b44d92eabc5351d52a17026ee991994711
Gokulan-S <gokulan383@gmail.com>
2024-07-19 18:17:22 +0530

    Create What's Your Name.py

---
+++ What's Your Name.py
@@ -0,0 +1,10 @@
+#  https://www.hackerrank.com/challenges/whats-your-name/
+
+def print_full_name(first, last):
+    message = f"Hello {first} {last}! You just delved into python."
+    print(message)
+
+if __name__ == '__main__':
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, last_name)
