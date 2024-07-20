56d30a3c8da5ee71afbf4954e9e906c3970e0f41
Gokulan-S <gokulan383@gmail.com>
2024-07-20 19:10:02 +0530

    Create Set .difference() Operation.py

---
+++ Set .difference() Operation.py
@@ -0,0 +1,11 @@
+#  https://www.hackerrank.com/challenges/py-set-difference-operation/
+
+n = int(input()) 
+english_subscribers = set(map(int, input().split()))
+
+m = int(input())
+french_subscribers = set(map(int, input().split()))
+
+only_english_subscribers = english_subscribers.difference(french_subscribers)
+
+print(len(only_english_subscribers))
