e6ba80c0d6d22dd04448f848ab0a3ad2f7fdb8cd
Gokulan-S <gokulan383@gmail.com>
2024-07-20 19:03:26 +0530

    Create Symmetric Difference.py

---
+++ Symmetric Difference.py
@@ -0,0 +1,15 @@
+#  https://www.hackerrank.com/challenges/symmetric-difference/
+
+def symmetric_difference(a, b):
+    return sorted(a.symmetric_difference(b))
+
+if __name__ == '__main__':
+    # Read input values
+    M = int(input())
+    a = set(map(int, input().split()))
+    N = int(input())
+    b = set(map(int, input().split()))
+    
+    result = symmetric_difference(a, b)
+    for element in result:
+        print(element)
